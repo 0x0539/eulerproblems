9c3a88b88bbe26a3d3db52c9fdf1fe0f77b60e10
Sebastian Goodman <sebastian.goodman@gmail.com>
2012-12-02 16:48:42 -0800

    adding some comments

--- 0015/solution.cpp
+++ 0015/solution.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #include <iostream>
 #include <vector>
 
+// we assume 2 sets of size l, though the concept can be generalized
 long multisetPermutations(long l){
   vector<int> numeratorTerms;
   vector<int> denominatorTerms;
@@ -19,9 +20,9 @@ long multisetPermutations(long l){
 
   bool worked = false;
 
+  // this loop cancels out terms in the permutation fraction, probably suboptimally
   do{
     worked = false;
-
     for(int i = numeratorTerms.size() - 1; i >= 0; i--){
       for(int j = denominatorTerms.size() - 1; j >= 0; j--){
         int n = numeratorTerms[i], d = denominatorTerms[j];

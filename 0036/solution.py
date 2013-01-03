7d2145799486fd7a6fead39aae9257487f589d36
Sebastian Goodman <sebastian@stylesaint.com>
2013-01-02 16:21:29 -0800

    36

---
+++ 0036/solution.py
@@ -0,0 +1,18 @@
+def digits(i, base):
+  while i > 0:
+    yield i % base
+    i /= base
+
+def is_palindrome(array):
+  for i in xrange(len(array)/2):
+    if array[i] != array[len(array)-1-i]:
+      return False
+  return True
+
+s = 0
+for i in xrange(1000000):
+  binary = [digit for digit in digits(i, 2)]
+  decimal = [digit for digit in digits(i, 10)]
+  if is_palindrome(binary) and is_palindrome(decimal):
+    s += i
+print(s)
